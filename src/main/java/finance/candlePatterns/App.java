f2a65d2fcd985a558fed1b394a6f024e4d59a77c
Denis M. Gabaidulin <gabaden@gmail.com>
2011-03-22 23:10:29 +0300

    + simple app

---
+++ src/main/java/finance/candlePatterns/App.java
@@ -0,0 +1,35 @@
+package finance.candlePatterns;
+
+import java.io.File;
+import java.util.List;
+
+import finance.candlePatterns.Core.Bar;
+import finance.candlePatterns.Core.FilterApplier;
+import finance.candlePatterns.Core.LongVolatilityBreakoutFilter;
+import finance.candlePatterns.IO.DataLoader;
+import finance.candlePatterns.IO.FinamDataParser;
+import finance.candlePatterns.IO.FinamParserFactory;
+import finance.candlePatterns.IO.ParserFactory;
+
+/**
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        ParserFactory<FinamDataParser> pFactory =
+            new FinamParserFactory<FinamDataParser>();
+        
+        DataLoader<FinamDataParser> loader = new DataLoader<FinamDataParser>(
+            new File("/home/sherman/stat/SPFB.RTS_010101_110322.txt"),
+            pFactory
+        );
+        
+        List<Bar> bars = loader.load();
+        
+        FilterApplier applier = new FilterApplier(
+            new LongVolatilityBreakoutFilter(bars, 0.4)
+        );
+        
+        applier.filtrate(bars);
+    }
+}
