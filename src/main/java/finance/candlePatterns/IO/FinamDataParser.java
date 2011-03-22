3fa7cc4a81557e18e5a9f9ebe486b27d0384b870
Denis M. Gabaidulin <gabaden@gmail.com>
2011-03-22 16:38:00 +0300

    + quick-n-dirty final data parser

--- src/main/java/finance/candlePatterns/IO/FinamDataParser.java
+++ src/main/java/finance/candlePatterns/IO/FinamDataParser.java
@@ -1,10 +1,37 @@
 package finance.candlePatterns.IO;
 
+import java.math.BigDecimal;
+
+import org.joda.time.LocalDateTime;
+
 import finance.candlePatterns.Core.Bar;
 
 public class FinamDataParser implements DataParser {
+    private final String DATA_SPLITTER = ",";
+    
     @Override
     public Bar getBarFromString(String parseString) {
-        return null;
+        String[] parts = parseString.split(DATA_SPLITTER);
+        
+        if (parts.length != 11)
+            throw new IllegalArgumentException("I don't know such format!");
+        
+        BigDecimal open = new BigDecimal(parts[2]);
+        BigDecimal high = new BigDecimal(parts[3]);
+        BigDecimal low = new BigDecimal(parts[4]);
+        BigDecimal close = new BigDecimal(parts[5]);
+        
+        if (high.intValue() < low.intValue())
+            throw new IllegalArgumentException("High must be greater than low!");
+        
+        Bar bar = new Bar(
+            new LocalDateTime(parts[0]),
+            open,
+            close,
+            high,
+            low
+        );
+        
+        return bar;
     }
 }
