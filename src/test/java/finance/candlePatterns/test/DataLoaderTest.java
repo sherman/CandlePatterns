988c37a8aab233e9638515b6101188ef7fb911b6
Denis M. Gabaidulin <gabaden@gmail.com>
2011-03-22 15:48:34 +0300

    * data loader test improved

--- src/test/java/finance/candlePatterns/test/DataLoaderTest.java
+++ src/test/java/finance/candlePatterns/test/DataLoaderTest.java
@@ -1,7 +1,9 @@
 package finance.candlePatterns.test;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.*;
@@ -9,9 +11,25 @@ import static org.testng.AssertJUnit.*;
 import finance.candlePatterns.IO.DataLoader;
 
 public class DataLoaderTest {
+    private final Logger log = Logger.getLogger(DataLoaderTest.class);
+    
     @Test
     public void testFileOpen() {
-        DataLoader loader = new DataLoader(new File("non existance file"));
+        // test on non existent file
+        DataLoader loader = new DataLoader(new File("non existent file"));
+        assertEquals(true, loader.load().isEmpty());
+        
+        // test real file
+        File testfile = null;
+        
+        try {
+            testfile = File.createTempFile("DataLoader", "test");
+        } catch (IOException e) {
+            log.error(e);
+            return;
+        }
+        
+        loader = new DataLoader(testfile);
         assertEquals(true, loader.load().isEmpty());
     }
 }
